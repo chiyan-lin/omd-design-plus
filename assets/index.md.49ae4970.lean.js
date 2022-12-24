deb2e224d7a4923698f4d54ee658fb1dd77cf4fb
chiyan-lin <chiyan-lin@users.noreply.github.com>
2022-12-24 16:40:18 +0000

    website deploy

--- assets/index.md.49ae4970.lean.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t}from"./app.4892cde5.js";const c='{"title":"OMD Design","description":"","frontmatter":{"title":"OMD Design","lang":null,"page":true},"headers":[],"relativePath":"index.md","lastUpdated":1671899089000}',a={},i=Object.assign(a,{__name:"index",setup(n){return(s,r)=>(e(),t("div"))}});export{c as __pageData,i as default};

ffcb8ab784a777cbfeccb20ad810552284603e38
chiyan-lin <chiyan-lin@users.noreply.github.com>
2022-12-24 16:45:54 +0000

    website deploy

---
+++ assets/docs_resource_index.md.94d864c5.lean.js
@@ -0,0 +1,1 @@
+import{r as t,o as a,c as o,a as r}from"./app.cbf03a3a.js";const l='{"title":"","description":"","frontmatter":{"page":true,"lang":"zh-CN"},"headers":[],"relativePath":"docs/resource/index.md","lastUpdated":1671900220000}',s={},p=Object.assign(s,{__name:"index",setup(n){return(c,_)=>{const e=t("Resource");return a(),o("div",null,[r(e)])}}});export{l as __pageData,p as default};
